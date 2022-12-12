3cfe894da2fe9dfc5acc92874d01b8ae1a39b377
한석규 <hansg0325@gmail.com>
2022-12-13 00:12:53 +0900

    404 수정

--- common/views.py
+++ common/views.py
@@ -18,6 +18,3 @@ def signup(request):
 
 def page_not_found(request, exception):
     return render(request, 'common/404.html', {})
-
-def server_error(request, exception):
-    return render(request, 'common/500.html', {})
